085bc6c0311c1fc68014700fba6aedf566aa4969
Meikostocs <cirillocristianpio02@gmail.com>
2025-04-15 00:15:58 +0200

    Edited gitignore

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+mkdir vivado
+
+cat <<EOF >docker-compose.yaml
+version: '3.9'
+
+services:
+  pc:
+    build: 
+      context: .
+      args:
+        SETUP_FILE: vivado2023_2.bin
+    environment:
+      - DISPLAY=${DISPLAY}
+      - XAUTHORITY=/tmp/.docker.xauth
+    ports:
+      - "2227:22"
+    volumes:
+      - $(pwd)/vivado:/opt:rw
+      - /tmp/.X11-unix:/tmp/.X11-unix:rw
+      - /dev:/dev               
+      - /run/udev:/run/udev          
+      - ${XAUTHORITY:-/tmp/.docker.xauth}:/tmp/.docker.xauth
+    privileged: true                 
+    network_mode: "host"            
+    ipc: host
+    stdin_open: true
+    tty: true
+EOF
+
+echo "[+] Generated docker-compose"
